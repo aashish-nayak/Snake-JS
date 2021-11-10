4bbc1947c89d9723c004deb86b9b864679a842cf
Aashish Nayak <ashunayakme@gmail.com>
2021-11-10 18:01:39 +0530

    fix

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@ self.addEventListener("install", (e) => {
   console.log("[Service Worker] Install");
   e.waitUntil(
     caches.open('sw-cache').then((cache)=>{
-        return cache.add('index.html');
+        return cache.add('./index.html');
     })
   );
 });
